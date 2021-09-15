341e464e83918da9242a85822a34f1b53b591251
Luizagalf <luizagalf@gmail.com>
2021-09-15 12:04:42 +0200

    Wordlist is ugprated

--- src/components/Wordlist/Wordlist.jsx
+++ src/components/Wordlist/Wordlist.jsx
@@ -6,23 +6,25 @@ import Wordlistitem from '../Wordlistitem/Wordlistitem';
 
 const Wordlist = () => {
     return (
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <td colspan="3" className={styles.title}>Word list</td>
-                </tr>
-                <tr>
-                    <th>English</th>
-                    <th>Russian</th>
-                    <th></th>
-                </tr>
-            </thead>
-            {
-                jsonWords.map((word) =>
-                    <Wordlistitem key={word.id} id={word.id} english={word.english} russian={word.russian}></Wordlistitem>
-                )
-            }
-        </table >
+        <div className={styles.list}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td colspan="3" className={styles.title}>Word list</td>
+                    </tr>
+                    <tr>
+                        <th>English</th>
+                        <th>Russian</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                {
+                    jsonWords.map((word) =>
+                        <Wordlistitem key={word.id} id={word.id} english={word.english} russian={word.russian}></Wordlistitem>
+                    )
+                }
+            </table>
+        </div>
     );
 }
 
