0a4f2abc5e58d983d4b65053a4a7e879a82d3f29
Luiza Galyautdinova <luizagalf@gmail.com>
2022-10-24 22:15:41 +0200

    Удален лишний код и добавлено описание

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,40 @@
 import styles from "./header.module.scss";
-import {
-    Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className={styles.header}>
-            <ul className={styles.menu}>
-                <li><Link to="/" className={styles.namesite}>Let's learn English!</Link></li>
-                <li><Link disabled to="#" className={styles.mainli}>Words</Link>
-                    <ul className={styles.smallul}>
-                        <li><Link to="/wordlist" className={styles.smallli}>Words list</Link></li>
-                        <li><Link to="/allwords" className={styles.smallli}>See all words</Link></li>
-                    </ul>
-                </li>
-                <li><Link to="/game" className={styles.mainli}>Repeat words</Link></li>
-            </ul>
-            <div className={styles.form}>
-                <input type="search" name="q" placeholder="What to search?" className={styles.search} />
-                <button className={styles.searchbutton}>Search</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.header}>
+      <ul className={styles.menu}>
+        <li>
+          <Link to="/" className={styles.namesite}>
+            Let's learn English!
+          </Link>
+        </li>
+        <li>
+          <Link disabled to="#" className={styles.mainli}>
+            Words
+          </Link>
+          <ul className={styles.smallul}>
+            <li>
+              <Link to="/wordlist" className={styles.smallli}>
+                Words list
+              </Link>
+            </li>
+            <li>
+              <Link to="/allwords" className={styles.smallli}>
+                See all words
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/game" className={styles.mainli}>
+            Repeat words
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
